90c9185663bdf2608ebf54f66f471ceb45b0bdf6
Nishant Chintala <crnishant@gmail.com>
2020-02-18 14:06:00 -0500

    added script for jumbotron

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(function(){
-    $("#go").on("click", function(){
-        
+    $("#go").on("submit", function(e){
+        e.preventDefault(); //prevents page reload
+        var first = $("#fname").val();
+        var last = $("#lname").val();
+        var newHTML = '<h1>'+first+' '+last+'</h1>';
+        $(".jumbotron").removeClass('d-none').html(newHTML);
+        return false; //prevent reload on submit
     })
 });
